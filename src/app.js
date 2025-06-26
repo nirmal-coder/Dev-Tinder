733e3771dbabe55f8acf266ac2e5843471ce9daf
nirmal-coder <keytowisdom33@gmail.com>
2025-06-26 11:45:13 +0530

    created signin , login , getProfile Api's

--- src/app.js
+++ src/app.js
@@ -8,73 +8,10 @@ const cookieParser = require("cookie-parser");
 const authUser = require("./middlewares/auth");
 const app = express();
 
-const port = process.env.PORT || 1207;
-app.use(express.json());
-app.use(cookieParser());
+const port = 1207;
 
-//  Sign in user
-
-app.post("/signin", async (req, res) => {
-  const { firstName, lastName, emailId, password, age, gender } = req.body;
-  try {
-    const isValidPassword = isStrongPassword(password);
-    if (!isValidPassword) throw new Error("Enter a Strong password!");
-    else {
-      const hashPassword = await bcrypt.hash(password, 10);
-      const user = new User({
-        firstName,
-        lastName,
-        emailId,
-        password: hashPassword,
-        age,
-        gender,
-      });
-
-      await user.save();
-      res.send("Signup Successfully!");
-    }
-  } catch (error) {
-    res.send(error.message);
-  }
-});
-
-// /Login user
-
-app.post("/login", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-    const user = await User.findOne({ emailId: emailId });
-    if (!user) throw new Error("Invalid credentials!");
-    const isValidPassword = await user.isPasswordValid(password);
-    if (!isValidPassword) throw new Error("Invalid Password!");
-    const token = await user.getJwt(user._id);
-    res.cookie("token", token, {
-      httpOnly: true,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-    res.send("login Success😁");
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-
-app.get("/profile", authUser, async (req, res) => {
-  try {
-    const user = await User.findOne({ _id: req.userId });
-    if (!user) throw new Error("User Not Found");
-    else {
-      res.send(user);
-    }
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
-
-connectDB().then(() => {
-  console.log("Database connected successfully....");
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}.......`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.......`);
 });
 
 app.use("/", (req, res) => {
